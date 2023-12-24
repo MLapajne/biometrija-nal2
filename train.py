0decd81ed9b7799b56aa92a04114134593648868
MLapajne <46561711+MLapajne@users.noreply.github.com>
2023-12-24 15:17:30 +0100

    third

--- train.py
+++ train.py
@@ -1,49 +1,56 @@
 from ultralytics import YOLO
-import os, itertools, datetime
+import os, itertools, datetime, json
 
-# # Load a model
-# model = YOLO("yolov8n.yaml")  # build a new model from scratch
-# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
-# Move to the dir of this script        
-#dname = os.path.dirname(os.path.abspath(__file__))
-os.chdir('/kaggle/working/origin-data/yolov8-ears')
-
-# Load the model, feel free to try other models
+# Load a model
 model = YOLO("yolov8n.pt")
 
-# learning_rates = [xx, xx, xx]
-# dropouts = [xx, xx, xx, xx]
-# weight_decays = [xx, xx,xx]
+# Best hyperparameters (previously found)
+learning_rate = 0.001
+dropout = 0.2
+weight_decay = 0.0001
 
-learning_rates = [0.0001]
-dropouts = [0.01]
-weight_decays = [0.0005]
+# Data augmentation parameters
+augmentation_params = {
+    'hsv_h': 0.015,  # Small adjustment
+    'hsv_s': 0.35,   # Moderate adjustment for saturation
+    'hsv_v': 0.35,   # Moderate adjustment for value/brightness
+    'degrees': 10,  # Small rotation
+    'translate': 0.1,  # Moderate translation
+    'scale': 0.25,   # Moderate scaling
+    'shear': 10,    # Mild shearing
+    'perspective': 0.001,  # Mild perspective transformation
+    'flipud': 0.0,  # No vertical flipping
+    'fliplr': 0.5,  # 50% chance of horizontal flipping
+    'mosaic': 1.0,
+    'mixup': 0.1,   # Low mixup
+    'copy_paste': 0.1  # Low copy-paste
+}
 
-param_combinations = list(itertools.product(learning_rates, dropouts, weight_decays))
-
-# Dear colleagues, for the full list and explanations, refer to: https://docs.ultralytics.com/usage/cfg/#train
-for lr, dropout, wd in param_combinations:
-    print(lr, dropout, wd)
+# Training with best hyperparameters and augmentation parameters
+try:
     model.train(
         data="ears.yaml", 
         epochs=20, 
-        optimizer='SGD', 
+        optimizer='AdamW', 
         pretrained=True, 
         patience=3,
         plots=False,
         val=True,
-        augment=False,
+        augment=True,
         dropout=dropout,
-        lr0=lr,
+        lr0=learning_rate,
         lrf=0.2,
         momentum=0.937,
-        weight_decay=wd,
+        weight_decay=weight_decay,
         warmup_epochs=3,
         warmup_momentum=0.5,
         warmup_bias_lr=0.1,
         box=7.5,
-        cls=0.5
+        cls=0.5,
+        **augmentation_params
     )
-    metrics = model.val()  # It'll automatically evaluate the data you trained. 
+    # Save the model checkpoint
+except Exception as e:
+    print(f"An error occurred during training: {e}")
 
+# Optionally, you can also save the training results and parameters for future reference
